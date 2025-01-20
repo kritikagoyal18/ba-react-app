4b9433bfaccd87db3c2d9fd0fca320de89390bb3
Kritika Goyal <kritikagoyal@adobe.com>
2025-01-20 17:15:56 +0530

    FlightBookingForm static component added

---
+++ src/components/FlightBookingForm.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import "./FlightBookingForm.scss";
+
+const FlightBookingForm = () => {
+  const [tripType, setTripType] = useState("one-way");
+
+  return (
+    <div className="flight-booking-form">
+      <form>
+        <div className="form-group-row">
+          <div className="form-group">
+            <label htmlFor="tripType">Fare</label>
+            <select
+              id="tripType"
+              value={tripType}
+              onChange={(e) => setTripType(e.target.value)}
+            >
+              <option value="one-way">One Way</option>
+              <option value="return">Return Trip</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="from">From</label>
+            <input
+              type="text"
+              id="from"
+              placeholder="Enter departure station"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="to">To</label>
+            <input
+              type="text"
+              id="to"
+              placeholder="Enter arrival station"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="form-group-row">
+          <div className="form-group">
+            <label htmlFor="departureDate">Depart</label>
+            <input type="date" id="departureDate" required />
+          </div>
+          
+            <div className="form-group">
+              <label htmlFor="returnDate">Return</label>
+              <input type="date" id="returnDate" required />
+            </div>
+          
+          <div className="form-group">
+            <label htmlFor="travelClass">Travel Class</label>
+            <select id="travelClass">
+              <option value="economy">Economy</option>
+              <option value="business">Business</option>
+              <option value="first-class">First Class</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="passengers">Select Passengers</label>
+            <input
+              type="number"
+              id="passengers"
+              min="1"
+              placeholder="No. of passengers"
+              required
+            />
+          </div>
+        </div>
+
+        <button type="button" className="find-flights-button">
+          Find Flights
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default FlightBookingForm;
