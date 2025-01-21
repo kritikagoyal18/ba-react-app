669446b20387c6aa9ca08a02205feb678869ad40
pankaj2186 <pansingh@adobe.com>
2025-01-21 17:01:22 +0530

    Create CarouselItem.jsx

---
+++ src/components/CarouselItem.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Title from "./base/Title";
+import Text from "./base/Text";
+import ContentFragment from "./base/ContentFragment";
+import "./CarouselItem.scss";
+
+const { REACT_APP_HOST_URI } = process.env;
+
+
+const CarouselItem = ({ cf, navigate }) => {
+  const image = REACT_APP_HOST_URI + cf?.backgroundImage?._path;
+  const title = cf?.title;
+  const subtitle = cf?.subtitle;
+  const price = cf?.price;
+  const cta = cf?.cta;
+  const ctalink = cf?.ctaLink;
+
+  return (
+    <ContentFragment
+      cf={cf}
+      className="carousel-item"
+    >         
+      <div
+  className="carousel-caption"
+  style={{ backgroundImage: `url(${image})` }}>
+        <Title heading="h5" prop="title" className="color-dark">
+          {title}
+        </Title>
+        <Text
+          content={subtitle}
+          prop="description"
+          className="font-size-large"
+        />
+        <p>{price}</p>
+        <p>{cta}</p>
+        <p>{ctalink}</p>
+      </div>
+    </ContentFragment>
+  );
+};
+
+export default CarouselItem;
