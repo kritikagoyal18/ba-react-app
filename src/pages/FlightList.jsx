bc81629d7ded690854ed88e42b9f9e0068de0e30
pankaj2186 <pansingh@adobe.com>
2025-01-21 22:26:32 +0530

    Update FlightList.jsx

--- src/pages/FlightList.jsx
+++ src/pages/FlightList.jsx
@@ -1,9 +1,40 @@
 import React, { useState, useEffect, useMemo } from "react";
 import TeaserCard from "../components/TeaserCard";
 import { useChooseAFare } from "../api";
+import { useFlightPackageById } from "../api";
+import FlightPackage from "../components/FlightPackage";
+import "./FlightList.scss";
+
  
 const FlightList = () => {
   const [fetchTrigger, setFetchTrigger] = useState(true);
+  const navigate = useNavigate();
+ 
+  const fareTypes = [
+    "economy-basic",
+    "business"
+  ];
+
+  // Individual hooks for each fare type
+  const economyBasicPackage = useFlightPackageById("economy-basic", 'master', fetchTrigger);
+  const businessPackage = useFlightPackageById("business", 'master', fetchTrigger);
+
+  // Combine results in useEffect
+  useEffect(() => {
+    const allPackages = [
+      economyBasicPackage.data,
+      businessPackage.data
+    ].filter(data => data !== null);
+    
+    setPackages(allPackages);
+  }, [economyBasicPackage.data, businessPackage.data]);
+
+  useEffect(() => {
+    if (!searchParams.get("variation")) {
+      navigate("/?variation=master");
+    }
+  }, [searchParams, navigate]);
+ 
   const { data } = useChooseAFare("master", fetchTrigger);
   console.log("0", data);
 
@@ -11,6 +42,13 @@ const FlightList = () => {
     <>
       <TeaserCard cf={data}>
       </TeaserCard>
+      {packages.map((offer, index) => (
+        <FlightPackage
+          key={offer.packageId || index}
+          cf={offer}
+          setFetchTrigger={setFetchTrigger}
+        />
+      ))}
     </>
   );
 
