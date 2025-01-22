3cd24f0cec535c5a7d952bbda8851f35054feedb
pankaj2186 <pansingh@adobe.com>
2025-01-22 11:38:41 +0530

    Print info only If value is present

--- src/components/FlightPackage.jsx
+++ src/components/FlightPackage.jsx
@@ -47,7 +47,7 @@ const FlightPackage = ({cf, navigate}) => {
                                 <p className="price-pretext">per adult</p>
                             </div>
                         </div>
-                        {bagsInfo && (
+                        {bagsInfo?.html && (
                             <div class="flex flex-col mb-15">
                                 <h4> Bags</h4>
                                 <Text
@@ -57,7 +57,7 @@ const FlightPackage = ({cf, navigate}) => {
                                 />
                             </div>
                         )}
-                        {seatsInfo && (
+                        {seatsInfo?.html && (
                             <div class="flex flex-col mb-15">
                                 <h4>Seats</h4>
                                 <Text
@@ -67,7 +67,7 @@ const FlightPackage = ({cf, navigate}) => {
                                 />
                             </div>
                         )}
-                        {refundInfo && (
+                        {refundInfo?.html && (
                             <div class="flex flex-col mb-15">
                                 <h4>Changes and refunds</h4>
                                 <Text
@@ -77,7 +77,7 @@ const FlightPackage = ({cf, navigate}) => {
                                 />
                             </div>
                         )}
-                        {moreInformation && (
+                        {moreInformation?.html && (
                             <div class="flex flex-col mb-15">
                                 <Text
                                     content={moreInformation}
