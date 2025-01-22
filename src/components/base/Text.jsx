f52f4ffa6a4cd2895d4fbd3f23d9a11dfefaf38b
Sherest Mehta <sherestdutt@gmail.com>
2025-01-22 10:16:38 +0530

    fixrelativeimage path in RTE

--- src/components/base/Text.jsx
+++ src/components/base/Text.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import {getURI} from "../../utils";
 
 const Text = ({ children, content, className, prop, label, behavior }) => {
   const editorProps = {
     "data-aue-prop": prop,
     "data-aue-label": label || (prop && prop[0].toUpperCase() + prop.slice(1)),
   };
+
+  // Function to fix image paths
+  function fixImagePaths(html, baseUrl) {
+    // Replace all occurrences of src="/path" with src="http://baseurl/path"
+    return html.replace(/src="\/(.*?)"/g, (match, path) => {
+      return `src="${baseUrl}/${path}"`;
+    });
+  }
 
   let Component = null;
 
@@ -20,7 +29,7 @@ const Text = ({ children, content, className, prop, label, behavior }) => {
         {...editorProps}
         data-aue-type="richtext"
         className={className}
-        dangerouslySetInnerHTML={{ __html: content.html }}
+        dangerouslySetInnerHTML={{ __html: fixImagePaths(content.html, getURI(""))}}
       />
     );
   }
